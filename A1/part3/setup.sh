671343a20e684189d83b90cedce047964afa771d
Madhava Krishna <madhava20217@iiitd.ac.in>
2024-02-16 03:59:08 +0530

    done

--- A1/part3/setup.sh
+++ A1/part3/setup.sh
@@ -2,3 +2,6 @@
 sudo apt-get install rabbitmq-server
 sudo systemctl start rabbitmq-server
 sudo systemctl enable rabbitmq-server
+
+sudo rabbitmqctl add_user myuser mypassword
+rabbitmqctl set_permissions -p / myuser ".*" ".*" ".*"
